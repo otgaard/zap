849d45f515a501c388494d422693b3d8162d4add
Darren Otgaar <darren.otgaar@icloud.com>
2016-03-05 12:21:53 +0200

    - Added simple glfw3 app.

--- tools/log.hpp
+++ tools/log.hpp
@@ -1,17 +1,26 @@
 /* Created by Darren Otgaar on 2016/03/05. http://www.github.com/otgaard/zap */
-#ifndef ZAP_LOG_HPP_H
-#define ZAP_LOG_HPP_H
+#ifndef ZAP_LOG_HPP
+#define ZAP_LOG_HPP
 
-namespace zap {
-    enum class log_severity : char {
+#include <mutex>
+#include <memory>
+#include <iosfwd>
+#include <sstream>
+
+namespace zap { namespace tools {
+    enum class log_level : char {
         DEBUG,
         WARNING,
         ERROR
     };
 
     class log {
+    public:
+        template<log_level level, typename... Args> void print(Args... args);
 
+    private:
+        template<typename T, typename... Args> void print(T car, Args... cons);
     };
-}
+}}
 
-#endif //ZAP_LOG_HPP_H
+#endif //ZAP_LOG_HPP
--- zap_example.cpp
+++ zap_example.cpp
@@ -12,4 +12,29 @@ int main(int argc, char* argv[]) {
     auto p = &::on_error;
 
     glfwWindowHint(GLFW_SAMPLES, 8);
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    auto window = glfwCreateWindow(1280, 768, "zap_example", nullptr, nullptr);
+    if(!window) {
+        std::cerr << "Error creating window - terminating" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
 }
