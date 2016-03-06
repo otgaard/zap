4b437aa5b592e2c1adcecb7cc25a3f14226c82bd
Darren Otgaar <darren.otgaar@icloud.com>
2016-03-06 12:07:40 +0200

    - Started adding vector classes

---
+++ maths/maths.hpp
@@ -0,0 +1,22 @@
+/* Created by Darren Otgaar on 2016/03/06. http://www.github.com/otgaard/zap */
+#ifndef ZAP_MATHS_HPP
+#define ZAP_MATHS_HPP
+
+#include <cmath>
+#include <array>
+#include <cassert>
+#include <numeric>
+
+#define ZERR_IDX_OUT_OF_RANGE "Index out of range"
+#define ZERR_ARG_COUNT_INVALID "Incorrect number of values provided"
+
+// Type and size safe static initialiser
+template <typename T, size_t N>
+struct static_list : std::array<T, N> {
+    template <typename... Args>
+    constexpr static_list(Args... args) : std::array<T, N>( { args... } ) {
+        static_assert(sizeof...(args) == N, ZERR_ARG_COUNT_INVALID);
+    }
+};
+
+#endif //ZAP_MATHS_HPP
--- zap_example.cpp
+++ zap_example.cpp
@@ -2,14 +2,19 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+#include "maths/vec.hpp"
+#include "maths/vec3.hpp"
+
+using namespace zap::maths;
+
 static void on_error(int error, const char* description) {
     std::cerr << "Error code: " << error << "desc:" << description << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-    if(!glfwInit()) return -1;
+    glfwSetErrorCallback(::on_error);
 
-    auto p = &::on_error;
+    if(!glfwInit()) return -1;
 
     glfwWindowHint(GLFW_SAMPLES, 8);
     glfwWindowHint(GLFW_DEPTH_BITS, 24);
@@ -25,9 +30,33 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
+
+    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    glfwSwapInterval(0);
+
+    constexpr vec3<float> val1 = {1.0f, 2.0f, 3.0f};
+    constexpr vec3<float> val2 = {10.f, 20.f, 30.f};
+    constexpr vec3<float> r1 = val1 + val2 * val1 - val2;
+    static_assert(r1.x == 1 && r1.y == 22 && r1.z == 63, "Something went very, very wrong");
+    constexpr vec3<float> r2 = 10.0f*r1;
+    static_assert(r2.x == 10 && r2.y == 220 && r2.z == 630, "Something went very, very wrong");
+
+    constexpr vec3<float> foo = (10.0f*vec3<float>({{1.0f, 2.0f, 3.0f}}))/2.0f + 1.0f/vec3<float>({{20.0f, 100.0f, 200.0f}});
+    static_assert(foo.x == 5.05f, "This is amazing");
+
+    constexpr float bar = dot(foo, foo);
+    std::cerr << "DOT PRODUCT:" << bar << std::endl;
+    static_assert(bar < 350.855f, "More static here");
+
+    std::cerr << foo.x << ":" << foo.y << ":" << foo.z << std::endl;
+
+    std::cerr << r1.x << " " << r1.y << " " << r1.z << " " << std::endl;
+    std::cerr << r2.x << " " << r2.y << " " << r2.z << " " << std::endl;
+
+    for(const auto& r : val1) {
+        std::cerr << "Here:" << r << std::endl;
+    }
 
     while(!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
@@ -36,5 +65,7 @@ int main(int argc, char* argv[]) {
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
