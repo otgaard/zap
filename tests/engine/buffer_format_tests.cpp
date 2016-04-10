e023aac250fb44c503cc38e9b50cab665b066aa2
Darren Otgaar <darren.otgaar@icloud.com>
2016-04-10 12:28:20 +0200

    - Removed old test for Buffer formats

--- tests/engine/buffer_format_tests.cpp
+++ tests/engine/buffer_format_tests.cpp
@@ -14,15 +14,6 @@ using namespace zap::engine;
 
 TEST(ZapEngineBufferFormatTests, TypeTests) {
     static_assert(maths::pow2<10>::value == 2*2*2*2*2*2*2*2*2*2, "Whoa");
-
-    static_assert(size_t(buffer_access::bitfield::BA_GUARD) == maths::pow2<6>::value, "BA_GUARD must be the last value in buffer_access");
-    static_assert(buffer_access::BA_SIZE == maths::log2_pow2(size_t(buffer_access::bitfield::BA_GUARD)),
-                  "BA_SIZE must be set to the number of bits used in the bitfield");
-    constexpr auto last = maths::pow2<6>::value;
-    constexpr auto size = maths::log2_pow2(size_t(buffer_access::bitfield::BA_GUARD));
-    ASSERT_EQ(size_t(buffer_access::bitfield::BA_GUARD), last);
-    constexpr auto val = buffer_access::BA_SIZE; // not expanding correctly
-    ASSERT_EQ(val, size);
 }
 
 TEST(ZapEngineBufferFormatTests, PODTests) {
