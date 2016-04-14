c25a519f01f41e71c520c014a1f9d07b74690ee0
Darren Otgaar <darren.otgaar@icloud.com>
2016-04-14 09:20:36 +0200

    - Configured Mac OS X to use GLEW

--- engine/gl_api.hpp
+++ engine/gl_api.hpp
@@ -7,7 +7,8 @@
 
 namespace zap { namespace engine { namespace gl {
 #ifdef __APPLE__
-#include <OpenGL/gl3.h>
+//#include <OpenGL/gl3.h>
+#include <GL/glew.h>
 #elif __linux__
 #include <GL/glew.h>
 #include <GL/gl.h>
