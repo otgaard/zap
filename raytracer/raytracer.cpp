4fd782a2049d784f0d6b39d8506db5c02a54be20
Darren Otgaar <darren.otgaar@icloud.com>
2016-08-16 06:27:27 +0200

    - Wayward normalisation

--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -4,6 +4,7 @@
 #include <maths/geometry/ray.hpp>
 #include <maths/geometry/plane.hpp>
 #include <maths/geometry/sphere.hpp>
+#include <maths/io.hpp>
 
 using namespace zap::maths;
 using namespace zap::engine;
@@ -18,7 +19,7 @@ raytracer::~raytracer() {
 
 }
 
-static zap::maths::loop<float> height(-1.f, 2.f, -1.f);
+static zap::maths::loop<float> height(-1.8f, 1.6f, -1.f);
 
 std::vector<zap::engine::rgb888_t> raytracer::render(int w, int h) {
     const float inv_w = 1.f/w, inv_h = 1.f/h;
@@ -28,7 +29,7 @@ std::vector<zap::engine::rgb888_t> raytracer::render(int w, int h) {
 
     UNUSED(z_max);
 
-    const vec3f light_pos = normalise(vec3f(0,1000,0));
+    const vec3f light_pos = vec3f(0,2,-4.f);
     sphere<float> sphere1(vec3f(0.f,height.value,-4.f), .2f);
     plane<float> plane1(vec3f(0,-2.f,0.f), vec3f(0,1.f,0));
     ray<vec3f> ray1(vec3f(0,0,0), vec3f(0,0,-1.f));
@@ -56,14 +57,13 @@ std::vector<zap::engine::rgb888_t> raytracer::render(int w, int h) {
             if(int count = intersection(sphere1, ray1, roots.data())) {
                 auto P = ray1.position(count == 2 ? roots.y : roots.x);
                 auto N = normalise(P - sphere1.C);
-
                 auto ld = normalise(light_pos - P);
                 pixels[row_offset+c].set3(byte(25+max(dot(ld,N),0.f)*220),0,0);
             }
         }
     }
 
-    height += 0.001f;
+    height += 0.1f;
 
     return pixels;
 }
