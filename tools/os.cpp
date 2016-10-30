d8238888ed81e625e83221bfd152dddf79d12058
otgaard <darren.otgaar@icloud.com>
2016-10-30 11:36:20 +0200

    - Added switch for Windows OS

--- tools/os.cpp
+++ tools/os.cpp
@@ -3,32 +3,45 @@
 //
 
 #include "os.hpp"
+#ifndef WIN32
 #include <dirent.h>
+#else
+#include <windows.h>
+#endif
 #include <sys/stat.h>
+#include <GLFW/glfw3.h>
 
 using namespace zap;
 
 bool is_file(const std::string& filename) {
+#ifndef WIN32
     struct stat buf;
     return stat(filename.c_str(), &buf) == 0 && S_ISREG(buf.st_mode);
+#else
+	return false;
+#endif
 }
 
 bool is_dir(const std::string& path) {
+#ifndef WIN32
     struct stat buf;
     return stat(path.c_str(), &buf) == 0 && S_ISDIR(buf.st_mode);
+#else
+	return false;
+#endif
 }
 
 std::vector<std::string> get_files(const std::string& path) {
     std::vector<std::string> out;
 
-#ifdef _WIN32
+#ifdef WIN32
     HANDLE dir;
     WIN32_FIND_DATA file_data;
 
-    if((dir = FindFirstFile((directory + "/*").c_str(), &file_data)) == INVALID_HANDLE_VALUE) return;
+    if((dir = FindFirstFile((path + "/*").c_str(), &file_data)) == INVALID_HANDLE_VALUE) return out;
 
     do {
-        const string file_name = directory + "/" + file_data.cFileName;
+        const std::string file_name = path + "/" + file_data.cFileName;
         if(file_data.cFileName[0] == '.' || (file_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0) continue;
         out.push_back(file_name);
     } while(FindNextFile(dir, &file_data));
