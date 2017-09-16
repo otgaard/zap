d7c3f9e67798039b21155f3bbb98a668ed2770e8
otgaard <otgaardm@gmail.com>
2017-09-16 15:33:26 +0200

    - Added shader_builder to example

--- examples/zap_example/zap_example.cpp
+++ examples/zap_example/zap_example.cpp
@@ -3,9 +3,20 @@
 #include <maths/io.hpp>
 #include <tools/log.hpp>
 #include <host/GLFW/application.hpp>
+#include <engine/framebuffer.hpp>
+#include <engine/mesh.hpp>
+#include <graphics3/g3_types.hpp>
+#include <generators/geometry/geometry3.hpp>
+#include <renderer/render_context.hpp>
+#include <renderer/shader_builder.hpp>
+#include <engine/state_stack.hpp>
 
 using namespace zap;
 using namespace zap::maths;
+using namespace zap::engine;
+using namespace zap::renderer;
+
+using p3n3t2_gen = generators::geometry3<graphics::vtx_p3n3t2_t, primitive_type::PT_TRIANGLES>;
 
 class zap_example : public application {
 public:
@@ -21,13 +32,37 @@ public:
     void on_mousewheel(double xinc, double yinc) final;
 
 protected:
+    framebuffer shadow_map_;
+    std::unique_ptr<mesh_base> sphere_;
+    std::unique_ptr<render_context> context_;
+    state_stack rndr_state_;
 };
 
 bool zap_example::initialise() {
+    sphere_ = p3n3t2_gen::make_mesh(p3n3t2_gen::make_UVsphere(10, 30, 1.f, false));
+    if(!sphere_->is_allocated()) {
+        LOG_ERR("Failed to allocate sphere mesh");
+        return false;
+    }
+
+    if(!rndr_state_.initialise()) {
+        LOG_ERR("Failed to initialise render state");
+        return false;
+    }
+
+    builder_task<> task;
+    task.method = builder_task<>::lighting_method::LM_NONE;
+    task.diffuse_map = builder_task<>::texture_type::TT_NONE;
+    context_ = shader_builder::build_basic_lights(task);
+
     return true;
 }
 
 void zap_example::on_resize(int width, int height) {
+    auto proj_matrix = make_perspective(45.f, width/float(height), 1.f, 10.f);
+    auto mv_matrix = make_translation(0.f, 0.f, -5.f);
+    context_->set_parameter("mvp_matrix", proj_matrix * mv_matrix);
+    context_->set_parameter("colour", vec4f{1.f, 1.f, 0.f, 1.f});
 }
 
 void zap_example::on_mousemove(double x, double y) {
@@ -41,7 +76,11 @@ void zap_example::update(double t, float dt) {
 }
 
 void zap_example::draw() {
-
+    context_->bind();
+    sphere_->bind();
+    sphere_->draw();
+    sphere_->release();
+    context_->release();
 }
 
 void zap_example::shutdown() {
